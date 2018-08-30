f0438425ab27005c34160634dbc446506c643dfa
Pushpak Sharma <sharmatryhard@gmail.com>
2018-08-30 23:37:07 +0530

    till Rendering List !

--- Learning/learningwithreact/src/components/counter.jsx
+++ Learning/learningwithreact/src/components/counter.jsx
@@ -2,21 +2,32 @@ import React,{Component} from 'react';
 //import ReactDOM from 'react-dom';
 export default class counter extends Component{
     state = {
-        count : 0
+        count : 0,
     };
+    style = {
+        fontSize : 40,
+    };
+    getClassesBadges() {
+        let classes = "text-";
+        classes += this.state.count === 0 ? "success m-2" : "info m-2";
+        return classes;
+    }
+
+    formatCount(){
+        var {count} = this.state;
+        return (count === 0 ? "zero" : "one");
+    }
     render(){
         return(
             <React.Fragment>
-                <span>{this.formatCount()}</span>
+                <span style = {this.style} className={this.getClassesBadges()}>{this.formatCount()}</span>
+                {/* {u can use in line style like style = {{fontSize : 40}}} */}
                 <button>Increment</button>
             </React.Fragment>
 
         );
     }
-    formatCount(){
-        var {count} = this.state;
-        return (count === 0 ? "zero" : "no");
-    }
+    
 }
 
 //export default counter;
